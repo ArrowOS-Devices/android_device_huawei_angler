252e6eafe5792563c322092a4cdae5bf05b1bbd4
xmxrtyx <garikapatiamartya@gmail.com>
2020-08-07 15:11:53 +0000

    angler: Mention device maintainer

--- arrow_angler.mk
+++ arrow_angler.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, $(TOPDIR)vendor/arrow/config/common.mk)
-
+DEVICE_MAINTAINER := Amartya
 PRODUCT_NAME := arrow_angler
 PRODUCT_DEVICE := angler
 PRODUCT_BRAND := google
